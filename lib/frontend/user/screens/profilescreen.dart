431d921ffa99fa2f0464cbdbd4a78b45b1218832
Muflih002 <muhammedmuflih01.com>
2026-07-29 11:43:43 +0530

    Move client frontend to user frontend

---
+++ lib/frontend/user/screens/profilescreen.dart
@@ -0,0 +1,310 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:intl/intl.dart';
+
+import 'package:shop_aura/frontend/user/screens/auth/login/login.dart';
+import 'package:shop_aura/frontend/user/screens/widgets/profile/edit_details.dart';
+import 'package:shop_aura/frontend/theme/app_colors.dart';
+import 'package:shop_aura/backend/services/authServices.dart';
+
+
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  String name = "";
+  String email = "";
+  String phone = "";
+  String createdAt = "";
+
+  @override
+  void initState() {
+    super.initState();
+    loadUser();
+  }
+
+  Future<void> loadUser() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      name = prefs.getString("user_name") ?? "";
+      email = prefs.getString("user_email") ?? "";
+      phone = prefs.getString("user_phone") ?? "";
+      createdAt = prefs.getString("createdAt") ?? "";
+    if (name.isEmpty || email.isEmpty || phone.isEmpty) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => LoginPage()),
+      );
+    }
+    });
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 1,
+                      spreadRadius: 3,
+                      offset: Offset(1, 0.6),
+                    ),
+                  ],
+                ),
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        SizedBox(height: 10),
+                        Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Center(
+                            child: Text(
+                              "R",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 40,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          name,
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: AppColors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          email,
+                          style: TextStyle(
+                            color: Colors.black45,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                          ),
+                        ),
+                        SizedBox(height: 35),
+                        OutlinedButton(
+                          onPressed: () {
+                            
+                          },
+                          style: OutlinedButton.styleFrom(
+                            minimumSize: Size(double.infinity, 60),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            side: BorderSide(color: Colors.grey.shade300),
+                          ),
+                          child: Row(
+                            children: [
+                              Icon(Icons.inventory_2_outlined, size: 24),
+                              SizedBox(width: 12),
+                              Expanded(
+                                child: Text(
+                                  "My Orders",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                              Icon(Icons.arrow_forward_ios, size: 12),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        OutlinedButton(
+                          onPressed: () {},
+                          style: OutlinedButton.styleFrom(
+                            minimumSize: Size(double.infinity, 60),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            side: BorderSide(color: Colors.grey.shade300),
+                          ),
+                          child: Row(
+                            children: [
+                              Icon(Icons.map_outlined, size: 24),
+                              SizedBox(width: 12),
+                              Expanded(
+                                child: Text(
+                                  "Manage Addresses",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                              Icon(Icons.arrow_forward_ios, size: 12),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        OutlinedButton(
+                          onPressed: () {},
+                          style: OutlinedButton.styleFrom(
+                            minimumSize: Size(double.infinity, 60),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            side: BorderSide(color: Colors.grey.shade300),
+                          ),
+                          child: Row(
+                            children: [
+                              Icon(Icons.settings_outlined, size: 24),
+                              SizedBox(width: 12),
+                              Expanded(
+                                child: Text(
+                                  "Settings",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                              Icon(Icons.arrow_forward_ios, size: 12),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 10,),
+                        SizedBox(
+                          width: double.infinity,
+                          height: 50,
+                        child:ElevatedButton(
+                          onPressed:()async{await Authservices.logOut(context);},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: AppColors.danger,
+                            foregroundColor: Colors.white,
+                            elevation: 2,
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 24,
+                              vertical: 14
+                            )
+                          ),
+                          child:Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                              Icon(Icons.logout,color: AppColors.white,size: 24,),
+                              SizedBox(width: 10,),
+                              Text(
+                                "Logout",
+                                style: TextStyle(
+                                  fontSize: 16
+                                ),
+                                )
+                                ]
+                              ),
+                              Icon(Icons.arrow_forward_ios,size: 12,)
+                            ],
+                          ),
+                        ),
+                        ),
+                        SizedBox(height: 30),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 1,
+                      spreadRadius: 3,
+                      offset: Offset(1, 0.6),
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 10),
+
+                      Text(
+                        "Profile Details",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: AppColors.primary,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      OutlinedButton(
+                        onPressed: () {},
+                        style: OutlinedButton.styleFrom(
+                          minimumSize: Size(double.infinity, 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          side: BorderSide(
+                            color: const Color.fromARGB(255, 37, 36, 36),
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(width: 5),
+                            Icon(Icons.edit_outlined, size: 24),
+                            Text(
+                              "Edit profile",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 10,),
+                      Details(icon: Icons.person_outline, title: "Full Name", data: name),
+                      Details(icon: Icons.email_outlined, title: "Email Address", data: email),
+                      Details(icon: Icons.phone_iphone_rounded, title: "Phone Number", data: phone),
+                      Details(icon: Icons.calendar_today_outlined, title: "joined At", data: createdAt),
+                      Details(icon: Icons.location_on_outlined, title: "Work Address", data: "Perinthalmanna, Pulamanthole, 679323"),
+                      SizedBox(height: 10,)
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
